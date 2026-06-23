9cb1a51199f20d0929a3a3c396e326990358ca56
Jan Felix Langenbach <JanFelix.Langenbach@casageo.de>
2026-06-23 18:48:39 +0200

    Introduce experimental QGIS plugin

--- qgis/docs/conf.py
+++ qgis/docs/conf.py
@@ -7,27 +7,25 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 author = "casaGeo Data + Services GmbH"
-project = "casaGeoTools"
+project = "casaGeoTools QGIS Plugin"
 project_copyright = "2026, casaGeo Data + Services GmbH"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
 ]
 
-templates_path = ["_templates"]
+templates_path = ["_templates", "../../docs/_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_favicon = "_static/favicon.png"
-html_static_path = ["_static"]
+html_theme = "alabaster"
+html_favicon = "../../docs/_static/favicon.png"
+html_static_path = ["_static", "../../docs/_static"]
 
 # -- Autodoc configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
@@ -40,19 +38,10 @@ autodoc_typehints = "description"
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
 
 intersphinx_mapping = {
+    "casageotools": ("https://casageotools.readthedocs.io/en/stable/", None),
     "geopandas": ("https://geopandas.org/en/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "pandas": ("https://pandas.pydata.org/docs/", None),
     "qgis": ("https://qgis.org/pyqgis/master/", None),
     "shapely": ("https://shapely.readthedocs.io/en/stable/", None),
 }
-
-# -- Napoleon configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
-
-napoleon_custom_sections = [
-    "Input Columns",
-    "Output Columns",
-]
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
