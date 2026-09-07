bd91b6fb67facffea5ce042e4c8d769e34ff999c
Jan Felix Langenbach <JanFelix.Langenbach@casageo.de>
2026-09-07 20:37:46 +0200

    Make result JSON parsing more robust and modular

--- casageo/tools/_types.py
+++ casageo/tools/_types.py
@@ -14,36 +14,87 @@
 #
 #  SPDX-License-Identifier: Apache-2.0
 
-import uuid
 from collections.abc import Mapping, Sequence
 from datetime import datetime
-from typing import Any
+from typing import Any, Self, cast
+from uuid import UUID, uuid4
 
 import pandas as pd
 
 from ._errors import APIReturnTypeError, SubqueryError
 
 
+def _get_timestamp(
+    d: dict[str, Any], /, default: datetime | None = None
+) -> datetime | None:
+    try:
+        return datetime.fromisoformat(d["timestamp"])
+    except (KeyError, TypeError, ValueError):
+        return default
+
+
+def _get_request_id(d: dict[str, Any], /, default: UUID | None = None) -> UUID | None:
+    try:
+        return UUID(d["request_id"])
+    except (KeyError, TypeError, ValueError):
+        return default
+
+
 class CasaGeoResult:
     def __init__(
         self,
         *,
-        _data: dict,
+        _data: dict[str, Any],
         _error: Exception | None = None,
         _timestamp: datetime | None = None,
-        _uuid: uuid.UUID | None = None,
+        _uuid: UUID | None = None,
     ) -> None:
         self._data = _data
         self._error = _error
         self._timestamp = _timestamp if _timestamp is not None else datetime.now()
-        self._uuid = _uuid if _uuid is not None else uuid.uuid4()
+        self._uuid = _uuid if _uuid is not None else uuid4()
 
     def __bool__(self) -> bool:
         return self._error is None
 
     def __repr__(self) -> str:
         classname = type(self).__qualname__
-        return f"<{classname} {self._uuid} [{'OK' if self else repr(self._error)}]>"
+        return f"<{classname} {self._uuid} [{'OK' if self else repr(cast(Exception, self._error))}]>"
+
+    @classmethod
+    def from_json(
+        cls,
+        json: Any,
+        *,
+        index: int | None = None,
+        timestamp: datetime | None = None,
+        uuid: UUID | None = None,
+    ) -> Self:
+        if not isinstance(json, dict):
+            msg = f"Invalid JSON response type: {json.__class__.__name__}"
+            raise APIReturnTypeError(msg)
+
+        value = json.get("value", {})
+        if not isinstance(value, dict):
+            msg = f"Invalid JSON response value type: {value.__class__.__name__}"
+            raise APIReturnTypeError(msg)
+
+        if isinstance(e := json.get("error"), dict):
+            error = SubqueryError(
+                e.get("message", repr(e)),
+                code=e.get("code"),
+                index=index,
+                details=e.get("details"),
+            )
+        else:
+            error = None
+
+        return cls(
+            _data=value,
+            _error=error,
+            _timestamp=_get_timestamp(json, timestamp),
+            _uuid=_get_request_id(json, uuid),
+        )
 
     def timestamp(self) -> datetime:
         """
@@ -54,7 +105,7 @@ class CasaGeoResult:
         """
         return self._timestamp
 
-    def uuid(self) -> uuid.UUID:
+    def uuid(self) -> UUID:
         """
         Return the UUID of this response object.
 
@@ -63,7 +114,7 @@ class CasaGeoResult:
         """
         return self._uuid
 
-    def json(self) -> dict:
+    def json(self) -> dict[str, Any]:
         """
         Return the raw API response as a dictionary.
 
@@ -102,25 +153,19 @@ class MultiResult[T: CasaGeoResult](Sequence[T]):
             raise APIReturnTypeError(msg)
 
         self._json = json
-        self._uuid = (
-            uuid.UUID(rqid) if (rqid := json.get("request_id")) else uuid.uuid4()
-        )
-        self._timestamp = (
-            datetime.fromisoformat(rqts)
-            if (rqts := json.get("timestamp")) and isinstance(rqts, str)
-            else datetime.now()
-        )
-
-        if result_type is not None:
-            self.ResultType = result_type
-
+        self._timestamp = _get_timestamp(json) or datetime.now()
+        self._uuid = _get_request_id(json) or uuid4()
         self._ids = list(ids)
         self._options = dict(options)
         self._results = [
-            self._make_result(i, r) for i, r in enumerate(self._json.get("results", []))
+            result_type.from_json(
+                result,
+                index=index,
+                timestamp=self._timestamp,
+                uuid=self._uuid,  # NOTE: Should this be the correlation ID?
+            )
+            for index, result in enumerate(self._json.get("results", []))
         ]
-
-        assert len(self._ids) == len(self._results)
 
     def __getitem__(self, index):
         return self._results[index]
@@ -133,34 +178,28 @@ class MultiResult[T: CasaGeoResult](Sequence[T]):
 
     def __repr__(self) -> str:
         classname = type(self).__qualname__
-        results = list(zip(self._ids, self._results, strict=True))
+        results = [
+            ((self._ids[index] if index < len(self._ids) else index), result)
+            for index, result in enumerate(self._results)
+        ]
         return f"<{classname} {self._uuid} {results!r}>"
 
-    def _make_result(self, index: int, data: Any):
-        assert isinstance(data, dict)
-        return self.ResultType(
-            _data=data.get("value", {}),
-            _error=SubqueryError(
-                e.get("message", repr(e)),
-                code=e.get("code"),
-                index=index,
-                details=e.get("details"),
-            )
-            if (e := data.get("error"))
-            else None,
-            _timestamp=self._timestamp,
-            _uuid=self._uuid,  # NOTE: Should this be the correlation ID?
-        )
-
     def _make_dataframe(self, index: int, result: CasaGeoResult):
+        try:
+            id_ = self._ids[index]
+        except IndexError:
+            id_ = index
+
         # noinspection PyArgumentList
-        df = result.dataframe(id_=self._ids[index], error_info=True, **self._options)
+        df = result.dataframe(id_=id_, error_info=True, **self._options)
+
         if (err := result.error()) and not df.empty:
             df["error_code"] = getattr(err, "code", "generic")
             df["error_message"] = str(err)
         # elif not df.empty:
         #     df["error_code"] = None
         #     df["error_message"] = None
+
         return df
 
     def timestamp(self) -> datetime:
@@ -172,7 +211,7 @@ class MultiResult[T: CasaGeoResult](Sequence[T]):
         """
         return self._timestamp
 
-    def uuid(self) -> uuid.UUID:
+    def uuid(self) -> UUID:
         """
         Return the UUID of the API response.
 
